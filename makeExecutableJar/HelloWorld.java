48052e566b0c7b8aa7866c7d39ee22ad0d1e3476
Harish Kayarohanam <technews.full@gmail.com>
2014-02-13 12:29:50 +0530

    Update HelloWorld.java

--- makeExecutableJar/HelloWorld.java
+++ makeExecutableJar/HelloWorld.java
@@ -1,4 +1,6 @@
 public class HelloWorld {
+    
+    static { System.out.println("static block of is initialised indicating that the class was initialised somewhere"); }
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
