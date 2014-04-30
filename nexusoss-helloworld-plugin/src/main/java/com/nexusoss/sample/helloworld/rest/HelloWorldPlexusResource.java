b6cf9901da9364c719c6f470cf2d0ebd4789efe1
Harish Kayarohanam <technews.full@gmail.com>
2014-04-30 10:34:03 +0530

    added hello world plugin for nexus oss

---
+++ nexusoss-helloworld-plugin/src/main/java/com/nexusoss/sample/helloworld/rest/HelloWorldPlexusResource.java
@@ -0,0 +1,67 @@
+package com.nexusoss.sample.helloworld.rest;
+
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.sonatype.plexus.rest.resource.PlexusResource;
+import org.sonatype.plexus.rest.resource.AbstractPlexusResource;
+import org.sonatype.plexus.rest.resource.PathProtectionDescriptor;
+import org.codehaus.plexus.component.annotations.Component;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+/**
+ * A sample rest resoruce to get you started. By default this will automatically be mounted at:
+ * http://localhost:8081/nexus/service/local/sample/hello
+ */
+@Named
+public class HelloWorldPlexusResource
+    extends AbstractPlexusResource
+    implements PlexusResource
+{
+	@Inject
+	private Logger log;
+	
+    @Override
+    public Object getPayloadInstance()
+    {
+    	log.info(" ************getPayloadInstance ");
+        // if you allow PUT or POST you would need to return your object.
+        return null;
+    }
+
+    @Override
+    public PathProtectionDescriptor getResourceProtection()
+    {
+    	log.info(" ************getResourceProtection ");
+        // to be controled by a new prermission
+        // return new PathProtectionDescriptor( this.getResourceUri(), "authcBasic,perms[nexus:somepermission]" );
+
+        // for an anonymous resoruce
+        return new PathProtectionDescriptor( this.getResourceUri(), "anon" );
+    }
+
+    @Override
+    public String getResourceUri()
+    {
+    	log.info(" ************getResourceUri ");
+        // note this must start with a '/'
+        return "/sample/hello";
+    }
+
+    @Override
+    public Object get( Context context, Request request, Response response, Variant variant )
+        throws ResourceException
+    {
+
+        // you can basically return any object, and it will be serialized
+
+        // we will keep it simple
+    	log.info(" ************get ");
+        return "Hello Sample Resource";
+    }
+}
